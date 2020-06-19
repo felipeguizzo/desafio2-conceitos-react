5f16ab7dec8f5c5d1bc535e515d9e6b40f305145
felipeguizzo <guuizzo@hotmail.com>
2020-06-19 19:27:58 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -32,11 +32,13 @@ function App() {
 
   async function handleRemoveRepository(id) {
     
-    const response = await api.delete(`repositories/${id}`);
+    await api.delete(`repositories/${id}`);
     
     const repositoryIndex = repositories.findIndex(repository => repository.id == id);
 
     repositories.splice(repositoryIndex, 1);
+
+    setRepositories([...repositories]);
   }
 
   return (
